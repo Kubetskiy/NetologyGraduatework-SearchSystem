cb63c93f6cc935d9421d90ae43ac7e7dd8f591a1
Sergey Kubetskiy <kubetskiy@gmail.com>
2022-10-27 21:08:23 +0300

    Added solution for additional task

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,27 +1,50 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class BooleanSearchEngine implements SearchEngine {
     private final Set<String> stopWords;
-    private final DataHandler dataPreparationTool;
     private final Map<String, Set<IndexedPage>> allIndexedDocuments;
 
+    /**
+     Загружаем стоп-слова и проиндексированные в единую структуру файлы pdf
+     */
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        this.dataPreparationTool = new DataHandler();
+        DataHandler dataPreparationTool = new DataHandler();
         stopWords = dataPreparationTool.getStopWords();
         allIndexedDocuments = dataPreparationTool.pdfDataHandler(pdfsDir);
     }
 
+    /**
+     @param searchString Список поиска (может содержать стоп-слова)
+     @return Список, отсортированный суммарному количеству переданных слов на странице
+     */
     @Override
-    public List<PageEntry> search(String word) {
+    public List<PageEntry> search(String searchString) {
+        // Разбираем строку поиска, чистим от стоп-слов
+        String[] searchWords = searchString.split("\\P{IsAlphabetic}+");
+        List<String> words = Arrays.stream(searchWords)
+                .filter(word -> !stopWords.contains(word))
+                .collect(Collectors.toList());
+        // Сюда будем результаты поиска складывать
         List<PageEntry> response = new ArrayList<>();
+        // По названиям документов
         var documents = allIndexedDocuments.keySet();
         for (String pdfName : documents) {
+            // Страницы одного документа
             var pages = allIndexedDocuments.get(pdfName);
             for (IndexedPage page : pages) {
-                if (page.getWordDistribution().containsKey(word)) {
-                    response.add(new PageEntry(pdfName, page.getPage()+1, page.getWordDistribution().get(word)));
+                // На каждой старнице для всех слов из поиска
+                int count = 0;
+                for (String word : words) {
+                    if (page.getWordDistribution().containsKey(word)) {
+                        count += page.getWordDistribution().get(word);
+                    }
+                }
+                // Добавим страницу к результатам если счетчик не нулевой
+                if (count > 0) {
+                    response.add(new PageEntry(pdfName, page.getPage() + 1, count));
                 }
             }
         }
